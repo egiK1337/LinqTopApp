4f45dd762340021f942160c123b21058ae95139e
egiK1337 <117719087+egiK1337@users.noreply.github.com>
2024-03-20 14:15:31 +0300

    add overload for TOP

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace LinqTopApp
+{
+    internal class Program
+    {
+
+        public class Person
+        {
+            public int Age { get; set; }
+            public string Name { get; set; }
+        }
+
+        static void Main()
+        {
+            var listPersons = new List<Person>()
+            {
+                new Person { Name = "Alice", Age = 30 },
+                new Person { Name = "Bob", Age = 25 },
+                new Person { Name = "Charlie", Age = 35 }
+            };
+
+            var list = new List<string>() { "1", "2", "3", "4", "5" };
+
+            var l1 = list.Top(19);
+
+            var l2 = listPersons.Top(34, person => person.Age);
+
+        }
+    }
+}
---
+++ TopGen.cs
@@ -0,0 +1,44 @@
+﻿
+namespace LinqTopApp
+{
+    public static class TopGen
+    {
+        public static IEnumerable<T> Top<T>(this IEnumerable<T> data, int percent)
+        {
+            if (percent < 1 || percent > 100)
+            {
+                throw new ArgumentException("Можно вводить число в диапазоне от 1 до 100");
+            }
+
+            var count = Math.Ceiling(data.Count() * (percent / 100.0));
+
+            if (Convert.ToInt32(count) != Convert.ToDouble(count))
+            {
+                count++;
+            }
+
+            var order = data.OrderByDescending(x => x);
+
+            return order.Take((int)count);
+        }
+
+        public static IEnumerable<T> Top<T, TKey>(this IEnumerable<T> data, int percent, Func<T, TKey> fieldSelector)
+        {
+            if (percent < 1 || percent > 100)
+            {
+                throw new ArgumentException("Можно вводить число в диапазоне от 1 до 100");
+            }
+
+            var count = Math.Ceiling(data.Count() * (percent / 100.0));
+
+            if (Convert.ToInt32(count) != Convert.ToDouble(count))
+            {
+                count++;
+            }
+
+            var order = data.OrderByDescending(fieldSelector);
+
+            return order.Take((int)count);
+        }
+    }
+}
